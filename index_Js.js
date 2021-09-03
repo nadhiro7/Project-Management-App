8397482d5bbcafea0b0a6bbd8f1e8103ad556f04
nadhiro <nadhirja@gmail.com>
2021-09-03 23:06:31 +0100

    first commit

--- index_Js.js
+++ index_Js.js
@@ -1,22 +1,21 @@
-let sl = document.querySelectorAll('.eleslider');
-let d = document.querySelectorAll('.dot');
-function changeAct(k){
-    for(let i = 0 ; i < sl.length ; i++)
-        sl[i].classList.remove('active_2');
-    for(j of d)
-        j.classList.remove('active_3');
-    sl[k].classList.add('active_2');
-    d[k].classList.add('active_3');
-}
+
 function dotClick1(){
-    changeAct(0);
+    document.documentElement.style.setProperty('--prColor' , "#9c27b0");
+    document.documentElement.style.setProperty('--btnBg', '#9b27b0a8');
+    document.documentElement.style.setProperty('--brBtn', '#9b27b0e0');
 }
 function dotClick2(){
-    changeAct(1);
+    document.documentElement.style.setProperty('--prColor' , "#1e2ba4");
+    document.documentElement.style.setProperty('--btnBg', '#1e2ba4a8');
+    document.documentElement.style.setProperty('--brBtn', '#1e2ba4e0');
 }
 function dotClick3(){
-    changeAct(2);
+    document.documentElement.style.setProperty('--prColor' , "#e91e63");
+    document.documentElement.style.setProperty('--btnBg', '#e91e63a8');
+    document.documentElement.style.setProperty('--brBtn', '#e91e63e0');
 }
 function dotClick4(){
-    changeAct(3);
+    document.documentElement.style.setProperty('--prColor' , "#ff5722");
+    document.documentElement.style.setProperty('--btnBg', '#ff5722a8');
+    document.documentElement.style.setProperty('--brBtn', '#ff5722e0');
 }
